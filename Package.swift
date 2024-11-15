989413077eb595fbd62c3b545e75e2ca7f1b3824
Zryte <Zryte@gmail.com>
2024-11-15 17:47:15 +0700

    Release v2.0.0-beta01

--- Package.swift
+++ Package.swift
@@ -14,27 +14,27 @@ let package = Package(
         .binaryTarget(
             name: "AmityUIKit",
             url: "https://sdk.amity.co/sdk-release/ios-uikit-frameworks/2.0.0-beta01/AmityUIKit.xcframework.zip",
-            checksum: "f75079e2a249518ac2a3de41685b8a11b1ec5ebc4de23e3a2edc846ae82255dc"
+            checksum: "bcb39132b15623599edcb54f2af0b5414651c4d21109dfa58488aa85be0de236"
         ),
         .binaryTarget(
             name: "AmitySDK",
             url: "https://sdk.amity.co/sdk-release/ios-uikit-frameworks/2.0.0-beta01/AmitySDK.xcframework.zip",
-            checksum: "13a479c1af606f9d943b3327435e865c10a50696d3536b8a42efdd8867a69eb9"
+            checksum: "5f63fe82a93578a96ccc9dd6fee73fd48d5938991dd468347c404cadeeb4106b"
         ),
         .binaryTarget(
             name: "Realm",
             url: "https://sdk.amity.co/sdk-release/ios-uikit-frameworks/2.0.0-beta01/Realm.xcframework.zip",
-            checksum: "ae19b3c7135bef5c332d75d8c4156f3d9d7fe08f714dd0519c502aecab816ca3"
+            checksum: "eed8a9fac3789e16af4670de7ef3baa9bc193f5a3191a376eb88f177b188ee25"
         ),
         .binaryTarget(
             name: "RealmSwift",
             url: "https://sdk.amity.co/sdk-release/ios-uikit-frameworks/2.0.0-beta01/RealmSwift.xcframework.zip",
-            checksum: "365694efc40b396985e41671447ca3a50e935d8aed3badeb9622beb856ff3467"
+            checksum: "74d5a7e001d2fa863327789a84827b8943901740567d1777b16f517b058416ae"
         ),
         .binaryTarget(
             name: "AmityUIKit4",
             url: "https://sdk.amity.co/sdk-release/ios-uikit-frameworks/2.0.0-beta01/AmityUIKit4.xcframework.zip",
-            checksum: "1fe1fda1a468d1ebaa9b182338a614cbbb6585a19ee4c02e4b0f6ab6b08fdf71"
+            checksum: "1e83af49ce26e6a66e1073ae6ad93ee0dccf435a1c975a325e090e837e460318"
         )
     ]
 )
